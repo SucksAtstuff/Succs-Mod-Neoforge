5c9c7eb4eea04746bed602f3425bffff55d3fa3c
Succ <75032354+SucksAtstuff@users.noreply.github.com>
2025-06-03 21:07:34 +0200

    Added datagen for making crops and seeds compostable

---
+++ src/main/java/net/succ/succsmod/datagen/ModDataMapProvider.java
@@ -0,0 +1,26 @@
+package net.succ.succsmod.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.registries.datamaps.builtin.Compostable;
+import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
+import net.succ.succsmod.item.ModItems;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModDataMapProvider extends DataMapProvider
+{
+    protected ModDataMapProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider) {
+        super(packOutput, lookupProvider);
+    }
+
+    @Override
+    protected void gather() {
+
+        this.builder(NeoForgeDataMaps.COMPOSTABLES)
+                .add(ModItems.GARLIC.getId(), new Compostable(0.45f), false);
+
+    }
+}
+
