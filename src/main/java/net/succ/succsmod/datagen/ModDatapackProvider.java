8ced324ca40204353ecc1b8cac2fee394c41cf9c
Succ <75032354+SucksAtstuff@users.noreply.github.com>
2025-06-03 22:23:59 +0200

    Added the things needed for setting up world gen for stuff later on
    e.g. ore gen, trees, structures and etc.

--- src/main/java/net/succ/succsmod/datagen/ModDatapackProvider.java
+++ src/main/java/net/succ/succsmod/datagen/ModDatapackProvider.java
@@ -1,16 +1,25 @@
 package net.succ.succsmod.datagen;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.succ.succsmod.SuccsMod;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.succ.succsmod.worldgen.ModBiomeModifiers;
+import net.succ.succsmod.worldgen.ModConfiguredFeatures;
+import net.succ.succsmod.worldgen.ModPlacesFeatures;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
-    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder();
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacesFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
 
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
