6d3e07e5b2aee06e9f3284aed4141c251fd923f8
Succ <75032354+SucksAtstuff@users.noreply.github.com>
2025-06-04 00:07:54 +0200

    made sure all data files are correct by deleting all of them and generating new ones
    which in turn fixed a bug where sapphire randomly dropped from breaking stone blocks

--- src/main/java/net/succ/succsmod/loot/AddItemModifier.java
+++ src/main/java/net/succ/succsmod/loot/AddItemModifier.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -14,7 +15,9 @@ import net.neoforged.neoforge.common.loot.LootModifier;
 public class AddItemModifier extends LootModifier {
     public static final MapCodec<AddItemModifier> CODEC = RecordCodecBuilder.mapCodec(inst ->
             LootModifier.codecStart(inst).and(
-                    BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(e -> e.item)).apply(inst, AddItemModifier::new));
+                    BuiltInRegistries.ITEM.byNameCodec().fieldOf("item").forGetter(e -> e.item)
+            ).apply(inst, AddItemModifier::new)
+    );
 
     private final Item item;
 
@@ -30,12 +33,14 @@ public class AddItemModifier extends LootModifier {
 
     @Override
     protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        for (LootItemCondition condition : this.conditions){
-            if(!condition.test(context)){
+        for (LootItemCondition condition : conditions) {
+            if (!condition.test(context)) {
                 return generatedLoot;
             }
         }
+
         generatedLoot.add(new ItemStack(this.item));
         return generatedLoot;
     }
+
 }
