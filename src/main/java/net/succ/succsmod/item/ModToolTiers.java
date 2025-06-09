0f5818b2120d88ef5012150d1d88738c971f87a7
Succ <75032354+SucksAtstuff@users.noreply.github.com>
2025-06-09 20:07:40 +0200

    Tool tiers now work properly
    i.e Netherite can only mine sunstone and sapphire and then it follows the tiering system from the mod
    
    1. Atherium
    2. Malachite
    3. Ruby
    4. Sunstone and Sapphire

--- src/main/java/net/succ/succsmod/item/ModToolTiers.java
+++ src/main/java/net/succ/succsmod/item/ModToolTiers.java
@@ -7,24 +7,27 @@ import net.succ.succsmod.util.ModTags;
 
 public class ModToolTiers {
     // Register a new custom tool tier for Sapphire
-    public static final Tier SAPPHIRE = new SimpleTier(ModTags.Blocks.NEEDS_SAPPHIRE_TOOL,
-            1561,9.5F,3.0F,12, () -> Ingredient.of(ModItems.SAPPHIRE));
+    public static final Tier SAPPHIRE = new SimpleTier(
+            ModTags.Blocks.NEEDS_SAPPHIRE_TOOL,
+            1561, 9.5F, 3.0F, 12, () -> Ingredient.of(ModItems.SAPPHIRE));
 
     // Register a new custom tool tier for Sunstone
-    public static final Tier SUNSTONE = new SimpleTier(ModTags.Blocks.NEEDS_SUNSTONE_TOOL,
-            1561,9.5F,3.0F,12, () -> Ingredient.of(ModItems.SUNSTONE));
+    public static final Tier SUNSTONE = new SimpleTier(
+            ModTags.Blocks.NEEDS_SUNSTONE_TOOL,
+            1561, 9.5F, 3.0F, 12, () -> Ingredient.of(ModItems.SUNSTONE));
 
     // Register a new custom tool tier for Ruby
-    public static final Tier RUBY = new SimpleTier(ModTags.Blocks.NEEDS_RUBY_TOOL,
-            1561,10.0F,4.0F,14, () -> Ingredient.of(ModItems.RUBY));
+    public static final Tier RUBY = new SimpleTier(
+            ModTags.Blocks.NEEDS_RUBY_TOOL,
+            1600, 10.0F, 4.0F, 14, () -> Ingredient.of(ModItems.RUBY));
 
     // Register a new custom tool tier for Malachite
-    public static final Tier MALACHITE = new SimpleTier(ModTags.Blocks.NEEDS_MALACHITE_TOOL,
-            1854,10.5F,5.0F,16, () -> Ingredient.of(ModItems.MALACHITE));
+    public static final Tier MALACHITE = new SimpleTier(
+            ModTags.Blocks.NEEDS_MALACHITE_TOOL,
+            1854, 10.5F, 5.0F, 16, () -> Ingredient.of(ModItems.MALACHITE));
 
     // Register a new custom tool tier for Atherium
-    public static final Tier ATHERIUM = new SimpleTier(ModTags.Blocks.INCORRECT_FOR_ATHERIUM_TOOL,
-            2565,12.0F,5.0F,18, () -> Ingredient.of(ModItems.ATHERIUM));
-
-
+    public static final Tier ATHERIUM = new SimpleTier(
+            ModTags.Blocks.NEEDS_ATHERIUM_TOOL,
+            2565, 12.0F, 5.0F, 18, () -> Ingredient.of(ModItems.ATHERIUM));
 }
