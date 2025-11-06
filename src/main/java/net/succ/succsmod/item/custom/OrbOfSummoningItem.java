b922ff4e9a822a2c374a980004df491238e8ff1b
succ <Kalle170512@gmail.com>
2025-11-06 20:58:53 +0100

    added hover text to the orb of summoning

--- src/main/java/net/succ/succsmod/item/custom/OrbOfSummoningItem.java
+++ src/main/java/net/succ/succsmod/item/custom/OrbOfSummoningItem.java
@@ -1,6 +1,8 @@
 package net.succ.succsmod.item.custom;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -10,6 +12,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
@@ -17,9 +20,18 @@ import net.succ.succsmod.entity.ModEntities;
 import net.succ.succsmod.entity.custom.TjEntity;
 import net.succ.succsmod.sound.ModSounds;
 
+import java.util.List;
+
 public class OrbOfSummoningItem extends Item {
     public OrbOfSummoningItem(Properties props) {
         super(props);
+    }
+
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.literal("I wonder what happens, if you use it....").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 
     @Override
@@ -38,14 +50,13 @@ public class OrbOfSummoningItem extends Item {
 
                 //  Play the custom summon sound server-wide
                 serverLevel.playSound(
-                        null,                             // null = broadcast to everyone
+                        null, // null = broadcast to everyone
                         pos,
                         ModSounds.ORB_SUMMON.get(),
-                        SoundSource.MASTER,               // or HOSTILE if you prefer
+                        SoundSource.MASTER,
                         1.0F,
                         1.0F
                 );
-
 
                 if (player != null && !player.isCreative()) {
                     stack.shrink(1);
